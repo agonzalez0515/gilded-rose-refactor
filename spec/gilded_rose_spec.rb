ac2aa715acaa6bdc296343cd780899499d1e0133
Angelica Gonzalez <angelica@8thlight.com>
2019-09-04 13:19:32 -0700

    Update tests with new method name and add Guardfile

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,89 +1,94 @@
-require "spec_helper"
+# require "spec_helper"
 require "gilded_rose"
 
 describe GildedRose do
-  describe "#update_quality" do
-    it "degrades item quality" do
+  describe "#daily_item_update" do
+    it "degrades xem quality" do
       items = [Item.new("Milk", 5, 50)]
-      GildedRose.new(items).update_quality
+      GildedRose.new(items).daily_item_update
       expect(items[0].quality).to eq 49
     end
 
     it "quality degrades twice as fast when days are less than 0" do
       items = [Item.new("Milk", 0, 50)]
-      GildedRose.new(items).update_quality
+      GildedRose.new(items).daily_item_update
       expect(items[0].quality).to eq 48
     end
 
     it "reduces sell in days quantity" do
       items = [Item.new("Milk", 5, 50)]
-      GildedRose.new(items).update_quality
+      GildedRose.new(items).daily_item_update
       expect(items[0].sell_in).to eq 4
     end
 
     it "minimum quality is 0" do
       items = [Item.new("Milk", 0, 0)]
-      GildedRose.new(items).update_quality
+      GildedRose.new(items).daily_item_update
       expect(items[0].quality).to eq 0
     end
 
     it "max quality is 50" do
-      items = [Item.new("Aged Brie", 30, 50)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 50
+      items = [
+        Item.new("Aged Brie", 30, 49), 
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+      GildedRose.new(items).daily_item_update()
+
+      items.each do |item| 
+        expect(item.quality).to eq 50
+      end
     end
 
     it "does not age Sulfuras" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 0
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 80)]
+      GildedRose.new(items).daily_item_update()
+      expect(items[0].sell_in).to eq 5
     end
 
     it "does not degrade Sulfuras" do
       items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 80
     end
 
     it "increases quality for Aged Brie by 1" do
       items = [Item.new("Aged Brie", 30, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 31
     end
 
     it "increases quality for Aged Brie by 2 when days are less than 0" do
       items = [Item.new("Aged Brie", 0, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 32
     end
     
     it "increases quality for backstage passes by 1 for more than 10 days" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 30, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 31
     end
 
     it "increases quality for backstage passes by 2 for days between 5 and 10" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 32
     end
     
     it "increases quality for backstage passes by 3 for less than 5 days" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 33
     end
 
     it "does not increase quality for backstage passes over 50" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 50
     end
     
     it "drops quality to 0 when days are 0 for backstage passes" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 30)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).daily_item_update()
       expect(items[0].quality).to eq 0
     end
   end
